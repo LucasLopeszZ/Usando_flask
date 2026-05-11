80acb2d73c6f87a8b051350d9c3a8b17af683506
LucasLopeszZ <Lucazlopez076@gmail.com>
2026-05-10 22:07:47 -0300

    Projeto_att

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,6 @@ from flask import Flask
 app = Flask (__name__)
 
 from app.routes import homepage
+from app.routes import sobre
+from app.routes import contato
+from app.routes import servico
--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,15 @@ from flask import render_template
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/servico')
+def servico():
+    return render_template('servico.html')
